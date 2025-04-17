5b6885a2de3b2db610feaae809ee7ee544a0ea0e
lsiddiqsunny <msiddiq3@nd.edu>
2025-04-17 11:15:48 -0400

    gpt-4o-mini results

--- Evaluation/job.sh
+++ Evaluation/job.sh
@@ -4,10 +4,9 @@
 #$ -m abe            # Send mail when job begins, ends and aborts
 #$ -pe smp 1     # Specify parallel environment and legal core size
 #$ -q long           # Specify queue
-#$ -N  Salesforce_codegen-2B-mono
-
+#$ -N  multi-gpt-4o-mini
 
 
 export PATH="/afs/crc.nd.edu/user/m/msiddiq3/Public/codeql-home/codeql:$PATH"
 conda activate Franc
-python evaluation.py
+python 	codeql_job_runner.py
