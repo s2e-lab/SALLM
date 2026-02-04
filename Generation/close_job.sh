dfedc566fa48d9da2a7e9b233c896d60045bead5
Mohammed Latif Siddiq <msiddiq3@nd.edu>
2026-02-04 12:56:03 -0500

    updated with gemini and gpt results

--- Generation/close_job.sh
+++ Generation/close_job.sh
@@ -3,10 +3,11 @@
 #$ -m abe            # Send mail when job begins, ends and aborts
 #$ -pe smp 32     # Specify parallel environment and legal core size
 #$ -q long          # Specify queue
-#$ -N close_job_gemini_python
+#$ -N close_job_gpt_githubpython
 #$ -cwd             # Run in current directory
 
-# python3 gpt_model.py ../ProcessedFiles/dataset_nl_prompt_best.jsonl
+ python3 gpt_model.py ../ProcessedFiles/github-dataset_nl_prompt_best.jsonl
 
-export HF_HOME="/groups/jdasilv2/Latif/SALLM/.cache/huggingface"
-python3 gemini_model.py ../ProcessedFiles/dataset_nl_prompt_best.jsonl
+
+#export HF_HOME="/groups/jdasilv2/Latif/SALLM/.cache/huggingface"
+#python3 gemini_model.py ../ProcessedFiles/github-dataset_nl_prompt_best.jsonl
